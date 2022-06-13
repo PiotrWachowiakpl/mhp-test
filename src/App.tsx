7bc98031a5f40d9f2ec95ecd2b678c50d189d674
ChargeAhead <piotrwachowiak81@gmail.com>
2022-06-13 19:09:56 +0200

    check

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload with GitHub Actions!!!
+          Hello THERE 
         </p>
         <a
           className="App-link"
